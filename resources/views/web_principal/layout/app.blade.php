04bef420b56477362b8cb30ff4d9febc6e0a11c2
Jesus Moris <jesus@soluciontotal.cl>
2020-12-03 16:19:38 -0300

    Mas cambio para mejorar el insight

--- resources/views/web_principal/layout/app.blade.php
+++ resources/views/web_principal/layout/app.blade.php
@@ -31,7 +31,7 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-        <link rel="stylesheet" href="/vendors/linericon/style.css">
+        <link rel="stylesheet" href="/vendors/linericon/style.min.css">
         <link rel="stylesheet" href="/css/font-awesome.min.css">
         <link rel="stylesheet" href="/vendors/owl-carousel/owl.carousel.min.css">
         <link rel="stylesheet" href="/vendors/lightbox/simpleLightbox.css">
